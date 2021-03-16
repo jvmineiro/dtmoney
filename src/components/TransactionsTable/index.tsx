e59c74b3e15f68df4a7f5230785ad36a8c78ef9d
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-03-16 11:39:35 -0300

    TransactionTable feito

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Catergoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <td >Desenvolvimento de Website</td>
+                        <td className="deposit">R$12.000</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/02/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw"> - R$1.100</td>
+                        <td>Casa</td>
+                        <td>10/02/2021</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    );
+} 
