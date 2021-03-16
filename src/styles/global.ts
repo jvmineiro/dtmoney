626fb782d45ac52e54e5b6943a7faee95bbad9de
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-03-16 11:04:49 -0300

    Summary feito

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --background: #f0f2f5;
         --red: #e52e4D;
         --blue: #5429CC;
-        
+        --green: #33cc95;
         --blue-light: #6933FF;
 
         --text-title: #363f5f;
