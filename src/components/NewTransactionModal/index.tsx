bea429627e071c3120e48a2f57800c01c4c1396d
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-03-18 10:30:03 -0300

    Inserção de transaction na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -19,9 +20,11 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        console.log({
+        const data = ({
             title, value, category, type,
-        })
+        });
+
+        api.post('/transactions', data)
     }
 
     return (
